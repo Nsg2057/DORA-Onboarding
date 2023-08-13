558c4686afc189cb96616c3edea687132ac4cd59
neha galla <saisrineha@gmail.com>
2023-08-13 16:52:19 -0400

    JSON Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,32 +8,25 @@ import {CsvService} from "./csv.service";
 })
 export class AppComponent {
   title = 'DORA-Onboarding';
-  jsonData:JSON;
-  eamsAppId: String;
-  
+  jsonData:any;
+  // eamsAppId: String;
+
 
 
 constructor(private csvService:CsvService) {
   this.loadCsvData();
+
 }
 
   async loadCsvData() {
-    const csvFilePath = 'assets/Data.csv'; // Replace with your CSV file path
+    const csvFilePath = 'assets/Data.csv';
 
     this.csvService.getCsvData(csvFilePath).subscribe(csvData => {
       this.jsonData = this.csvService.parseCsvToJson(csvData);
-
-      console.log(this.jsonData); // JSON data from CSV
+      console.log(this.jsonData);
     });
   }
 
-  iterateJsonObject() {
-    const keys = Object.keys(this.myJsonObject);
-    for (const key of keys) {
-      const value = this.myJsonObject[key];
-      console.log(`Key: ${key}, Value: ${value}`);
-    }
-  }
 }
 
 
